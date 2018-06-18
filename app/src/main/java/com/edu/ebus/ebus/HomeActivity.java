c83330aafc3a29d4840f1ac440ade02ff14eaec3
MSI <chaovirun092@gmail.com>
2018-06-18 23:01:25 +0700

    [ebus-v:0.1]

---
+++ app/src/main/java/com/edu/ebus/ebus/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.edu.ebus.ebus;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                }
+                return false;
+            }
+        });
+    }
+}
