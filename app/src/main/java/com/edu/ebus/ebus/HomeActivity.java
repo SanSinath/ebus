45c9c30e042f789767a164a69dea8a225fef6839
MSI <chaovirun092@gmail.com>
2018-06-19 16:25:55 +0700

    [ebus-v:1.0.0] menu

--- app/src/main/java/com/edu/ebus/ebus/HomeActivity.java
+++ app/src/main/java/com/edu/ebus/ebus/HomeActivity.java
@@ -1,25 +1,64 @@
 package com.edu.ebus.ebus;
 
+import android.animation.LayoutTransition;
+import android.app.FragmentManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
-
+                    case R.id.mhome:
+                        HomeFragment homeFragment = new HomeFragment();
+                        FragmentManager fragmentManager = getFragmentManager();
+                        android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.framelayout, homeFragment);
+                        fragmentTransaction.commit();
+                        break;
+                    case R.id.mevent:
+                        EventsFragment eventsFragment = new EventsFragment();
+                        FragmentManager fragmentManager1 = getFragmentManager();
+                        android.app.FragmentTransaction transaction = fragmentManager1.beginTransaction();
+                        transaction.replace(R.id.framelayout, eventsFragment);
+                        transaction.commit();
+                        break;
+                    case R.id.mrecently:
+                        RecentlyFragment recentlyFragment = new RecentlyFragment();
+                        FragmentManager fragmentManager2 = getFragmentManager();
+                        android.app.FragmentTransaction transaction1 = fragmentManager2.beginTransaction();
+                        transaction1.replace(R.id.framelayout, recentlyFragment);
+                        transaction1.commit();
+                        break;
+                    case R.id.muser:
+                        UserFragment userFragment = new UserFragment();
+                        FragmentManager fragmentManager3 = getFragmentManager();
+                        android.app.FragmentTransaction fragmentTransaction1 = fragmentManager3.beginTransaction();
+                        fragmentTransaction1.replace(R.id.framelayout, userFragment);
+                        fragmentTransaction1.commit();
+                        break;
                 }
                 return false;
             }
         });
+
+
     }
 }
