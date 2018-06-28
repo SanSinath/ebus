c5f498043a8f6b4820c2425b8029618e894ed9e2
Navin IV <navinev1234@gmail.com>
2018-06-28 16:25:33 -0700

    [Commit Login form}

--- app/src/main/java/com/edu/ebus/ebus/MainActivity.java
+++ app/src/main/java/com/edu/ebus/ebus/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
     @Override
