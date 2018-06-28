0f40228127d66b6a699470c3c1236a1d0a7518c3
Navin IV <navinev1234@gmail.com>
2018-06-28 16:10:19 -0700

    Merge branch 'master' of D:\ebus with conflicts.

--- app/src/main/java/com/edu/ebus/ebus/MainActivity.java
+++ app/src/main/java/com/edu/ebus/ebus/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
+        new  Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this,HomeActivity.class);
