82995e2e16f10d91e0c0ef7e1ebfc88cc7934b45
SanSinath <sansianth168@gmail.com>
2018-07-17 13:25:57 +0700

    Verify PhoneNumber

--- app/src/main/java/com/edu/ebus/ebus/home/SetTicketActivity.java
+++ app/src/main/java/com/edu/ebus/ebus/home/SetTicketActivity.java
@@ -6,29 +6,94 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.edu.ebus.ebus.R;
 import com.edu.ebus.ebus.home.VerifyActivity;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class SetTicketActivity extends AppCompatActivity {
     private Button tbpay;
+    private EditText number_phone;
+    String phoneNumber;
+    FirebaseAuth mAuth;
+    String codesent;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.set_ticket);
+        mAuth = FirebaseAuth.getInstance();
         tbpay = findViewById (R.id.bt_set_booking);
+        number_phone = findViewById (R.id.txt_set_phonenumber);
+
+
 
         tbpay.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (getApplication (),VerifyActivity.class);
-                startActivity (intent);
+                sentverificationcode();
+                Toast.makeText (getApplication (),"phonnumber"+phoneNumber+"codesent"+codesent,Toast.LENGTH_LONG).show ();
             }
         });
-
-
     }
 
+    private void sentverificationcode(){
+
+        phoneNumber = number_phone.getText ().toString ();
+        Toast.makeText (getApplication (),"phoneumer"+phoneNumber,Toast.LENGTH_LONG).show ();
+        if (phoneNumber.isEmpty ()){
+            number_phone.setError ("Phone number is required");
+            number_phone.requestFocus ();
+            return;
+        }
+//        if (phoneNumber.length ()<10){
+//            number_phone.setError ("Please enter a valid phone");
+//            number_phone.requestFocus ();
+//            return;
+//        }
+        Toast.makeText (getApplication (),"callProvider",Toast.LENGTH_LONG).show ();
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+855"+phoneNumber,        // Phone number to verify
+                30,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+    }
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks () {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            Toast.makeText (getApplication (),"PhoneAuthcteadentail",Toast.LENGTH_LONG).show ();
+
+            Intent intent = new Intent (getApplication (),VerifyActivity.class);
+            intent.putExtra ("number_phone","0"+phoneNumber);
+            intent.putExtra ("codesent",codesent);
+            Toast.makeText (getApplication (),"phonnumber"+phoneNumber+"codesent"+codesent,Toast.LENGTH_LONG).show ();
+            startActivity (intent);
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent (s, forceResendingToken);
+            codesent = s;
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText (getApplication (),"PhoneAuthcteaFaild",Toast.LENGTH_LONG).show ();
+
+        }
+
+
+    };
 
 }
+
--- app/src/main/java/com/edu/ebus/ebus/home/VerifyActivity.java
+++ app/src/main/java/com/edu/ebus/ebus/home/VerifyActivity.java
@@ -1,19 +1,89 @@
 package com.edu.ebus.ebus.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.edu.ebus.ebus.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 /**
  * Created by USER on 7/12/2018.
  */
 
 public class VerifyActivity extends AppCompatActivity {
+
+    private TextView dispay_number;
+    private Button bt_sumit;
+    private String codesent;
+    private EditText entercode;
+    FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.code_verify_layout);
+
+        dispay_number= findViewById (R.id.txt_numer_verify);
+        bt_sumit = findViewById (R.id.btn_Submit);
+        entercode = findViewById (R.id.txt_enter_code_verify);
+
+
+        Intent intent = getIntent ();
+        String data = intent.getStringExtra ("number_phone");
+        codesent = intent.getStringExtra ("codesent");
+
+
+
+        Toast.makeText (getApplication (),"Intent data phone"+data,Toast.LENGTH_LONG).show ();
+        Toast.makeText (getApplication (),"Intent data code sent"+codesent,Toast.LENGTH_LONG).show ();
+        // Toast.makeText (getApplication (),"Intent data code enter"+code,Toast.LENGTH_LONG).show ();
+        dispay_number.setText (data);
+
+        bt_sumit.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                String code = entercode.getText ().toString ();
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codesent,code);
+                signInWithPhoneAuthCredential(credential);
+            }
+
+        });
     }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult> () {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText (getApplication (),"Verify succece",Toast.LENGTH_LONG).show ();
+
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+
+
+    }
+
 }
